50e747befb42497e7b515befdd81ab36221dc849
Veronica Melanie <150957620+VeronicaMelanie@users.noreply.github.com>
2026-09-17 11:37:37 +0300

    primul commit

---
+++ Exercitiul7/ModulA/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name" : "Extindere partener standard",
+    "version" : "19.0.1.0.0",
+    "category" : "Contacts",
+    "summary" : "Extindere partener standard",
+    "author" : "Veronica",
+    "license" : "LGPL-3",
+    "depends" : ["base"],
+    "data" : [
+        'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+    ],
+    "installable" : True,
+    "application" : True,
+}
